58989c0f12671e184ac19b0b6bb510b7f358096b
nasefaziz <nasifaziz855@gmail.com>
2026-08-19 14:42:25 +0200

    Add word guessing game information script
    This script provides information about a word guessing game, including word details, player name, type checks, character counts, and string manipulations.

---
+++ threee.py
@@ -0,0 +1,44 @@
+# Gime information 
+
+#verbaile
+Game_name = "Gusse the Word"
+Word = "python"
+plyaer = "aziz"
+
+print("----------------------------------")
+print("\t" + Game_name)
+print("👾👾👾👾👾👾👾👾👾👾👾👾👾👾")
+      
+print("----------------------------------")
+print("Word: " +  Word )
+print("plyaer: " +  plyaer)
+
+# typse 
+print("====TYPSE====")
+print(type(plyaer))
+print(type(Word))
+#index
+print("INDEX")
+print("first"+ Word [0])
+print("last"+ Word[-1])
+ 
+ #string 
+print("\n ====== WORD INFORMATION======")
+print("Cgaracters Count:" + str (len(Word)))
+
+print("letter   'o' Count:" + str (Word .count ("o")))
+print("starts . With p :" + str(Word.startswith("p")))
+print("End . wiht n :" + str(Word.endswith("n")))
+
+
+#spilt
+sentence = "I love python programming"
+print( "Words:" +
+str (sentence.split ()))
+massege = "hello " + plyaer + " I love  " + Word
+print("\n " + massege)
+
+print("\n ==========================")
+print("\t Good lock!")
+
+#Aziz.......................................................
